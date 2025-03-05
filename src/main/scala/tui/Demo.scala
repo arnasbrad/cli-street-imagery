0147f6f05bd29b9fd49c1397b7f9e5b612b16f1e
IgnasSu <150551487+IgnasSu@users.noreply.github.com>
2025-03-05 14:32:01 +0200

    can walk through tabs

--- src/main/scala/tui/Demo.scala
+++ src/main/scala/tui/Demo.scala
@@ -1,54 +1,96 @@
-package tui
+package tuiexamples
 
 import tui._
 import tui.crossterm.CrosstermJni
-import tui.withTerminal
+import tui.widgets.tabs.TabsWidget
+import tui.widgets.BlockWidget
 
-import java.time.{Duration, Instant}
-import scala.Ordering.Implicits._
+object TabsExample {
+  case class App(
+                  titles: Array[String],
+                  var index: Int = 1
+                ) {
 
-object Demo {
+    def changeTab(nextTab : String) : Unit =
+      nextTab match {
+        case "config" => index = 0
+        case "streetview" => index = 1
+        case "help" => index = 2
+      }
+  }
   def main(args: Array[String]): Unit = withTerminal { (jni, terminal) =>
-    // create app and run it
-    val tick_rate = Duration.ofMillis(250)
-    val app = App(title = "Crossterm Demo", enhanced_graphics = true)
 
-    run_app(terminal, app, tick_rate, jni)
+    // create app and run it
+    val app = App(titles = Array("Config", "StreetView", "Help"))
+    run_app(terminal, app, jni);
   }
 
-  def run_app(terminal: Terminal, app: App, tick_rate: java.time.Duration, jni: CrosstermJni): Unit = {
-    var last_tick = Instant.now()
-
-    def elapsed = java.time.Duration.between(last_tick, java.time.Instant.now())
-    def timeout = {
-      val timeout = tick_rate.minus(elapsed)
-      new tui.crossterm.Duration(timeout.toSeconds, timeout.getNano)
-    }
-
+  def run_app(terminal: Terminal, app: App, jni: CrosstermJni): Unit = {
+    var curentState = "streetview"
+    var pastState = ""
     while (true) {
-      terminal.draw(f => ui.draw(f, app))
+      terminal.draw(f => ui(f, app))
 
-      if (jni.poll(timeout)) {
-        jni.read() match {
-          case key: tui.crossterm.Event.Key =>
-            key.keyEvent.code match {
-              case char: tui.crossterm.KeyCode.Char => app.on_key(char.c())
-              case _: tui.crossterm.KeyCode.Left    => app.on_left()
-              case _: tui.crossterm.KeyCode.Up      => app.on_up()
-              case _: tui.crossterm.KeyCode.Right   => app.on_right()
-              case _: tui.crossterm.KeyCode.Down    => app.on_down()
-              case _                                => ()
+      jni.read() match {
+        case key: tui.crossterm.Event.Key =>
+          key.keyEvent.code match {
+            case char: tui.crossterm.KeyCode.Char if char.c() == 'q' => return
+            case char: tui.crossterm.KeyCode.Char if char.c() == 'h' && curentState == "streetview"=> {
+              app.changeTab("help")
+              pastState = curentState
+              curentState = "help"
             }
-          case _ => ()
-        }
-      }
-      if (elapsed >= tick_rate) {
-        app.on_tick()
-        last_tick = Instant.now()
-      }
-      if (app.should_quit) {
-        return
+            case char: tui.crossterm.KeyCode.Char if char.c() == 'b' && curentState == "help" || curentState == "config" => {
+              app.changeTab(pastState)
+              val x = pastState
+              pastState = curentState
+              curentState = x
+            }
+            case char: tui.crossterm.KeyCode.Char if char.c() == 'c' && curentState == "streetview"=> {
+              app.changeTab("config")
+              pastState = curentState
+              curentState = "config"
+            }
+            case _                                                   => ()
+          }
+        case _ => ()
       }
     }
+  }
+
+  def ui(f: Frame, app: App): Unit = {
+    val chunks = Layout(
+      direction = Direction.Vertical,
+      margin = Margin(5, 5),
+      constraints = Array(Constraint.Length(3), Constraint.Min(0))
+    ).split(f.size)
+
+    val block = BlockWidget(style = Style(bg = Some(Color.White), fg = Some(Color.Black)))
+    f.renderWidget(block, f.size)
+    val titles = app.titles
+      .map { t =>
+        val (first, rest) = t.splitAt(1)
+        Spans.from(
+          Span.styled(first, Style(fg = Some(Color.Yellow))),
+          Span.styled(rest, Style(fg = Some(Color.Green)))
+        )
+      }
+
+    val tabs = TabsWidget(
+      titles = titles,
+      block = Some(BlockWidget(borders = Borders.ALL, title = Some(Spans.nostyle("Tabs")))),
+      selected = app.index,
+      style = Style(fg = Some(Color.Cyan)),
+      highlightStyle = Style(addModifier = Modifier.BOLD, bg = Some(Color.Black))
+    )
+    f.renderWidget(tabs, chunks(0))
+    val inner = app.index match {
+      case 0 => BlockWidget(title = Some(Spans.nostyle("Inner 0")), borders = Borders.ALL)
+      case 1 => BlockWidget(title = Some(Spans.nostyle("Inner 1")), borders = Borders.ALL)
+      case 2 => BlockWidget(title = Some(Spans.nostyle("Inner 2")), borders = Borders.ALL)
+      case 3 => BlockWidget(title = Some(Spans.nostyle("Inner 3")), borders = Borders.ALL)
+      case _ => sys.error("unreachable")
+    }
+    f.renderWidget(inner, chunks(1))
   }
 }
