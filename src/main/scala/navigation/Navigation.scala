f6faa8e413fff16c256569371b046f6c1a17dced
Arnas Bradauskas <45391480+arnasbrad@users.noreply.github.com>
2025-03-07 17:43:54 +0200

    Making a Runner class which will be used in UI (#9)

--- src/main/scala/navigation/Navigation.scala
+++ src/main/scala/navigation/Navigation.scala
@@ -6,86 +6,75 @@ import clients.mapillary.{Errors, MapillaryClient}
 import clients.mapillary.Models.{ImageData, MapillaryImageId}
 import common.Models.{Coordinates, Radius}
 
-trait Navigation {
+sealed trait Navigation {
   def findPossibleNavigationOptions(
       currentImageId: MapillaryImageId,
       currentCoordinates: Coordinates,
-      radius: Radius = Radius.unsafeCreate(5),
-      maxAmount: Int = 5
+      radius: Radius,
+      maxAmount: Int
   )(implicit
       mapillaryClient: MapillaryClient
   ): EitherT[IO, Errors.MapillaryError, List[ImageData]]
 }
 
-object Navigation {
-  sealed trait NavigationType
-  case object DistanceBased extends NavigationType
-
-  def make(navigationType: NavigationType): Navigation = {
-    navigationType match {
-      case DistanceBased => new DistanceBasedNavigation
-    }
-  }
-
+object Navigation extends Navigation {
   // given coordinates, find the closest surrounding images
-  private class DistanceBasedNavigation extends Navigation {
-    def findPossibleNavigationOptions(
-        currentImageId: MapillaryImageId,
-        currentCoordinates: Coordinates,
-        radius: Radius,
-        maxAmount: Int
-    )(implicit
-        mapillaryClient: MapillaryClient
-    ): EitherT[IO, Errors.MapillaryError, List[ImageData]] = {
-      for {
-        imagesResponse <- mapillaryClient.getImagesInfoByLocation(
-          currentCoordinates,
-          radius
+  def findPossibleNavigationOptions(
+      currentImageId: MapillaryImageId,
+      currentCoordinates: Coordinates,
+      radius: Radius,
+      maxAmount: Int
+  )(implicit
+      mapillaryClient: MapillaryClient
+  ): EitherT[IO, Errors.MapillaryError, List[ImageData]] = {
+    for {
+      imagesResponse <- mapillaryClient.getImagesInfoByLocation(
+        currentCoordinates,
+        radius
+      )
+
+      otherImages = imagesResponse.data.filter(_.id != currentImageId)
+
+      imagesWithDistance = otherImages.map { imageData =>
+        (
+          imageData,
+          calculateDistance(currentCoordinates, imageData.coordinates)
         )
+      }
 
-        otherImages = imagesResponse.data.filter(_.id != currentImageId)
+      // Sort by distance and take top maxAmount
+      closestImages = imagesWithDistance
+        .sortBy(_._2) // Sort by distance (second element of tuple)
+        .take(maxAmount)
+        .map(_._1) // Extract just the ImageData
 
-        imagesWithDistance = otherImages.map { imageData =>
-          (
-            imageData,
-            calculateDistance(currentCoordinates, imageData.coordinates)
-          )
-        }
-
-        // Sort by distance and take top maxAmount
-        closestImages = imagesWithDistance
-          .sortBy(_._2) // Sort by distance (second element of tuple)
-          .take(maxAmount)
-          .map(_._1) // Extract just the ImageData
-
-      } yield closestImages
-    }
-
-    // Helper function to calculate distance between two coordinates using Haversine formula
-    private def calculateDistance(
-        coord1: Coordinates,
-        coord2: Coordinates
-    ): Double = {
-      val earthRadius = 6371000 // Earth radius in meters
-
-      val lat1Rad    = Math.toRadians(coord1.lat)
-      val lat2Rad    = Math.toRadians(coord2.lat)
-      val latDiffRad = Math.toRadians(coord2.lat - coord1.lat)
-      val lngDiffRad = Math.toRadians(coord2.lng - coord1.lng)
-
-      val a = Math.sin(latDiffRad / 2) * Math.sin(latDiffRad / 2) +
-        Math.cos(lat1Rad) * Math.cos(lat2Rad) *
-        Math.sin(lngDiffRad / 2) * Math.sin(lngDiffRad / 2)
-
-      val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-      earthRadius * c // Distance in meters
-    }
+    } yield closestImages
   }
 
-  // TODO: maybe add more types, like sequence based. Currently my sequence logic was wrong, so I deleted it
-  /*
+  // Helper function to calculate distance between two coordinates using Haversine formula
+  private def calculateDistance(
+      coord1: Coordinates,
+      coord2: Coordinates
+  ): Double = {
+    val earthRadius = 6371000 // Earth radius in meters
+
+    val lat1Rad    = Math.toRadians(coord1.lat)
+    val lat2Rad    = Math.toRadians(coord2.lat)
+    val latDiffRad = Math.toRadians(coord2.lat - coord1.lat)
+    val lngDiffRad = Math.toRadians(coord2.lng - coord1.lng)
+
+    val a = Math.sin(latDiffRad / 2) * Math.sin(latDiffRad / 2) +
+      Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+      Math.sin(lngDiffRad / 2) * Math.sin(lngDiffRad / 2)
+
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    earthRadius * c // Distance in meters
+  }
+}
+
+// TODO: maybe add more types, like sequence based. Currently my sequence logic was wrong, so I deleted it
+/*
   private class SequenceBasedNavigation extends Navigation {
   }
-   */
-}
+ */
